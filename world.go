e06f06a97c6053315242af8b3fb789fc6cf1def7
whythat <whythat@protonmail.com>
2017-11-17 10:42:15 +0200

    Core: draft game object model

---
+++ world.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type BlockType = int
+
+const (
+	BlockTypes = 4
+
+	SimpleBlock = iota
+	SmallBTCBlock
+	BigBTCBlock
+	SurprizeMFKBlock
+)
+
+// Block is an integral part of the world.
+type Block struct {
+	Type      BlockType
+	Integrity int
+}
+
+// Cell is a pair of coordinates in a block grid
+type Cell struct {
+	X, Y int
+}
+
+// Digger is a main character of the game.
+type Digger struct {
+}
+
+func NewDigger() Digger {
+	return Digger{}
+}
+
+// World contains game state.
+type World struct {
+	Digger Digger
+	Grid   map[Cell]*Block
+}
+
+func NewWorld() World {
+	return World{
+		Digger: NewDigger(),
+		Grid:   make(map[Cell]*Block),
+	}
+}
+
+func (world World) GridView(min, max Cell) [][]*Block {
+	fmt.Printf("minc: %v, maxc: %v\n", min, max)
+	height, width := max.Y-min.Y, max.X-min.X
+	grid := make([][]*Block, height)
+	for i := 0; i < height; i++ {
+		grid[i] = make([]*Block, width)
+		if i+min.Y < 0 {
+			continue
+		}
+		for j := 0; j < width; j++ {
+			cell := Cell{Y: i + min.Y, X: j + min.X}
+			block, ok := world.Grid[cell]
+			if !ok {
+				block = &Block{
+					Type:      rand.Intn(BlockTypes),
+					Integrity: 4,
+				}
+				world.Grid[cell] = block
+			}
+			grid[i][j] = block
+		}
+	}
+	return grid
+}
+
+// Check whether there is a block at a given cell.
+func (world World) ContainsBlock(cell Cell) bool {
+	return !(world.Grid[cell] == nil)
+}
+
+// Kick a block with a hammer, decrementing its integrity.
+// When integrity falls down to 0, block dissapears.
+func (world World) HammerBlock(cell Cell) {
+	if world.Grid[cell].Integrity <= 0 {
+		world.Grid[cell] = nil
+	} else {
+		world.Grid[cell].Integrity--
+	}
+}
